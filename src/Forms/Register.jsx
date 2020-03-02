a1803beae49d65b2d1c9380b9c55566aed5b25ef
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-03-02 15:25:35 -0500

    New user register component

---
+++ src/Forms/Register.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import axiosWithAuth from "../Utils/AxiosWithAuth";
+
+import { Form, Button, Grid } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
+
+import GameImage from '../Images/King of the pixel.png'
+
+import RegisterSuccessAlert from "../Utils/AuthenticationAlerts/RegisterSuccessAlert";
+import RegisterErrorAlert from "../Utils/AuthenticationAlerts/RegisterErrorAlert";
+
+const Register = (props) => {
+  const [reg, setReg] = useState({
+    username: "",
+    password: ""
+  });
+
+  const [alert, setAlert] = useState({
+    alertMessage: ""
+  });
+
+  const handleChanges = e => {
+    setReg({
+      ...reg,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/auth/register", reg)
+      .then(res => {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("user_id", res.data.user.id);
+        props.history.push("/dashboard");
+        setAlert({ alertMessage: "successful" });
+      })
+      .catch(error => {
+        setAlert({ alertMessage: "unsuccessful" });
+      });
+  };
+
+  return (
+    <>
+      <div className="RegisterWindow">
+        <Link to='/'>
+          <img src={GameImage} alt='king of the pixel main' className='AuthScreenImage' />
+        </Link>
+
+        <Grid.Column textAlign="center" className="register-form-grid">
+          <Form onSubmit={onSubmit}>
+            <Form.Input
+              type="username"
+              name="username"
+              placeholder="Username"
+              onChange={handleChanges}
+            />
+
+            <Form.Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChanges}
+            />
+
+            <Button color="violet" fluid size="medium">
+              REGISTER
+            </Button>
+          </Form>
+
+          {alert.alertMessage === "successful" ? (
+            <RegisterSuccessAlert />
+          ) : null}
+          {alert.alertMessage === "unsuccessful" ? (
+            <RegisterErrorAlert />
+          ) : null}
+
+          <p className="RegisterBottomText">
+            Already Have An Account ?
+            <Link to="/login">
+              <p className="register-redirect-text">Login</p>
+            </Link>
+          </p>
+        </Grid.Column>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Register;
