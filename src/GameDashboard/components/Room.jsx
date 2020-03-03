8739df75b05c46abf02093bb9a74fa57b143214e
Tommy Coleman <tommiilee@gmail.com>
2020-03-03 18:24:26 -0500

    finished a basic version of movement for the sprite

--- src/GameDashboard/components/Room.jsx
+++ src/GameDashboard/components/Room.jsx
@@ -2,16 +2,28 @@ import React, { useState, useCallback, useEffect } from 'react'
 import KeyboardEventHandler from 'react-keyboard-event-handler';
 import RoomUnitFloor from './RoomUnitFloor'
 import RoomUnitWall from './RoomUnitWall'
+import knight from '../../Images/knight.png'
 const Room = () => {
-    const [roomUnits, setRoomUnits] = useState([1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, , 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0])
-    const [activeRoom, setActiveRoom] = useState(0)
+    const [roomUnits, setRoomUnits] = useState([[1, 1, 1, 1, 1, 0, 1, 1, 0, 1], [1, 0, 1, 0, 1, 1, 1, 1, 1, 1], [1, 1, 0, 1, 0, 1, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 0, 1, 1, 1, 1], [1, 1, 1, 0, 1, 1, 1, 1, 1, 1], [1, 1, 1, 0, 1, 1, 1, 1, 1, 1], [1, 0, 1, 1, 0, 1, 0, 1, 0, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 1, 1, 1, 1, 1, 1, 1, 1]])
+    const [activeRoom, setActiveRoom] = useState({ array: 0, index: 0 })
     const move = (event, key) => {
-        if (key == 'right') {
-            if (roomUnits[activeRoom + 1] == 1) {
-                setActiveRoom(activeRoom + 1)
-                setRoomUnits([...roomUnits])
-            }
+        let player = document.getElementById('player-unit')
+        let left_position = parseInt(player.style.left.split('px')[0])
+        let top_position = parseInt(player.style.top.split('px')[0])
+        if (key == 'right' && roomUnits[activeRoom.array][activeRoom.index + 1] == 1) {
+            setActiveRoom({ ...activeRoom, index: activeRoom.index + 1 })
+            player.style.left = `${left_position + 32}px`
+        } else if (key == 'left' && roomUnits[activeRoom.array][activeRoom.index - 1] == 1) {
+            setActiveRoom({ ...activeRoom, index: activeRoom.index - 1 })
+            player.style.left = `${left_position - 32}px`
+        } else if (key == 'up' && roomUnits[activeRoom.array - 1] != undefined && roomUnits[activeRoom.array - 1][activeRoom.index] == 1) {
+            setActiveRoom({ ...activeRoom, array: activeRoom.array - 1 })
+            player.style.top = `${top_position - 32}px`
+        } else if (key == 'down' && roomUnits[activeRoom.array + 1] != undefined && roomUnits[activeRoom.array + 1][activeRoom.index] == 1) {
+            setActiveRoom({ ...activeRoom, array: activeRoom.array + 1 })
+            player.style.top = `${top_position + 32}px`
         }
+        console.log(left_position)
     }
 
 
@@ -21,20 +33,18 @@ const Room = () => {
                 <KeyboardEventHandler
                     handleKeys={['down', 'right', 'left', 'up']}
                     onKeyEvent={(key, e) => move(e, key)} />
-                {
-                    roomUnits.map((unit, index) => {
-
+                <div id="player-unit" style={{ zIndex: '1', position: 'absolute', left: '8px', top: '8px' }}>
+                    <img src={knight} />
+                </div>
+                {roomUnits.map(array => {
+                    return array.map((unit, i) => {
                         if (unit == 1) {
-                            if (index == activeRoom) {
-                                return <RoomUnitFloor key={index} active={true} />
-                            } else {
-                                return <RoomUnitFloor key={index} active={false} />
-                            }
+                            return <RoomUnitFloor key={i} />
                         } else {
-                            return <RoomUnitWall key={index} />
+                            return <RoomUnitWall key={i} />
                         }
                     })
-                }
+                })}
             </div>
         </>
     )
--- src/GameDashboard/components/RoomUnitFloor.jsx
+++ src/GameDashboard/components/RoomUnitFloor.jsx
@@ -3,19 +3,13 @@ import '../../App.css';
 import floor from '../../Images/floor.jpg'
 
 import knight from '../../Images/knight.png'
-const RoomUnitFloor = (props) => {
-    const [directions, setDirections] = useState([])
-    const [isActive,] = useState(props.active)
-    const [cssClass, setCssClass] = useState("player-non-active")
-    useEffect(() => {
-        if (isActive == true) {
-            setCssClass('player-unit')
-        }
-    }, [isActive])
+const RoomUnitFloor = () => {
+
+
     return (
         <>
             <div className='room-unit'>
-                <img src={floor} className='room-unit-image' /><img src={knight} className={cssClass} />
+                <img src={floor} className='room-unit-image' />
 
             </div>
 
