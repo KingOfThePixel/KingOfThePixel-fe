aaad0c7520f1e9111d46642675a5eb6ce0e6813a
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-03-03 13:27:48 -0500

    Perpend the base url with the cors link

--- src/Utils/AxiosWithAuth.jsx
+++ src/Utils/AxiosWithAuth.jsx
@@ -4,7 +4,7 @@ export default function axiosWithAuth () {
   const token = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: 'https://kotp.herokuapp.com/',
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://kotp.herokuapp.com/',
     headers: {
       Authorization: token
     }
