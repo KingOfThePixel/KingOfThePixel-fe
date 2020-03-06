4bbfc811aaefe1b020a56558238b29e952ecd360
Tommy Coleman <tommiilee@gmail.com>
2020-03-05 20:37:19 -0500

    hooked up movement to backend calls

--- src/GameDashboard/components/Room.jsx
+++ src/GameDashboard/components/Room.jsx
@@ -5,29 +5,65 @@ import RoomUnitFloor from './RoomUnitFloor'
 import RoomUnitWall from './RoomUnitWall'
 import knight from '../../Images/knight.png'
 import king from '../../Images/king.png'
+import axiosWithAuth from '../../Utils/AxiosWithAuth';
 
 const Room = (props) => {
     const [roomUnits, setRoomUnits] = useState([[{ is_path: false }]])
-    const [activeRoom, setActiveRoom] = useState({ array: 0, index: 0 })
-    const [playerPosition, setPlayerPosition] = useState({ top: 8, left: 8 })
+    const [activeRoom, setActiveRoom] = useState({})
+    const [playerPosition, setPlayerPosition] = useState({ top: 0, left: 0 })
     let imageSrc = knight
     if (props.goblet == true) {
         imageSrc = king
         document.getElementById('player-unit').style.transition = 'left 2s, top 2s'
     }
     const move = (event, key) => {
-        if (key == 'right' && roomUnits[activeRoom.array][activeRoom.index].e_to.is_path === true) {
-            setActiveRoom({ ...activeRoom, index: activeRoom.index + 1 })
-            setPlayerPosition({ ...playerPosition, left: playerPosition.left + 32 })
-        } else if (key == 'left' && roomUnits[activeRoom.array][activeRoom.index].w_to.is_path === true) {
-            setActiveRoom({ ...activeRoom, index: activeRoom.index - 1 })
-            setPlayerPosition({ ...playerPosition, left: playerPosition.left - 32 })
-        } else if (key == 'up' && roomUnits[activeRoom.array][activeRoom.index].n_to.is_path === true) {
-            setActiveRoom({ ...activeRoom, array: activeRoom.array - 1 })
-            setPlayerPosition({ ...playerPosition, top: playerPosition.top - 32 })
-        } else if (key == 'down' && roomUnits[activeRoom.array][activeRoom.index].s_to.is_path === true) {
-            setActiveRoom({ ...activeRoom, array: activeRoom.array + 1 })
-            setPlayerPosition({ ...playerPosition, top: playerPosition.top + 32 })
+        if (key == 'right') {
+            axiosWithAuth()
+                .post('/api/adv/move', { direction: 'e' })
+                .then(east => {
+
+                    setPlayerPosition({ top: (east.data.y * 32), left: (east.data.x * 32) })
+                    setActiveRoom(roomUnits[east.y][east.x])
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        } else if (key == 'left') {
+            axiosWithAuth()
+                .post('/api/adv/move', { direction: 'w' })
+                .then(west => {
+
+                    setPlayerPosition({ top: (west.data.y * 32), left: (west.data.x * 32) })
+                    setActiveRoom(roomUnits[west.y][west.x])
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        } else if (key == 'up') {
+            axiosWithAuth()
+                .post('/api/adv/move', { direction: 'n' })
+                .then(north => {
+                    setPlayerPosition({ top: (north.data.y * 32), left: (north.data.x * 32) })
+                    setActiveRoom(roomUnits[north.y][north.x])
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        } else if (key == 'down') {
+            axiosWithAuth()
+                .post('/api/adv/move', { direction: 's' })
+                .then(south => {
+                    console.log(south)
+                    setPlayerPosition({ top: (south.data.y * 32), left: (south.data.x * 32) })
+                    setActiveRoom(roomUnits[south.y][south.x])
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
 
     }
@@ -37,18 +73,7 @@ const Room = (props) => {
             .get('/api/adv/maps')
             .then(matrix => {
                 setRoomUnits(matrix.data.map)
-                let spawn_rooms = []
-                matrix.data.map.forEach((array, j) => {
-                    array.forEach((room, i) => {
-                        if (room.is_spawn == true) {
-                            spawn_rooms.push({ array: j, index: i })
-                        }
-                    })
-                })
 
-                let player_position = spawn_rooms[Math.floor(Math.random() * spawn_rooms.length - 1)]
-                setActiveRoom(player_position)
-                setPlayerPosition({ top: playerPosition.top + (32 * player_position.array), left: playerPosition.left + (32 * player_position.index) })
             })
             .catch(err => {
                 console.log(err)
@@ -56,7 +81,9 @@ const Room = (props) => {
         AxiosWithAuth()
             .get('/api/adv/init')
             .then(response => {
-                console.log(response.data)
+                console.log(response)
+                setPlayerPosition({ top: (response.data.y * 32), left: (response.data.x * 32) })
+                setActiveRoom(roomUnits[response.y][response.x])
             })
             .catch(err => {
                 console.log(err)
@@ -75,6 +102,7 @@ const Room = (props) => {
                 </div>
                 {roomUnits.map((array, j) => {
                     return array.map((unit, i) => {
+                        console.log('x', unit.x, 'y', unit.y)
                         if (unit.is_path == true) {
                             return <RoomUnitFloor key={i} itemId={unit.item_id} />
                         } else {
